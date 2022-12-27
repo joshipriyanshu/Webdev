485bc8d4278573e7fb54bec34f78a446bc95e91f
joshipriyanshu <64037903+joshipriyanshu@users.noreply.github.com>
2022-12-28 00:57:28 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,88 @@
+// comments in javascript
+
+// declare a varibale
+
+var Myname= "priyanshu";
+
+var x;
+
+console.log(x)
+
+let lastname = "joshi";
+
+
+const pi=  3.14;
+
+console.log(Myname)
+
+
+var sum = 10 + 2;
+
+var difference = 10- 2 ;
+
+var multiply = 7 *8;
+
+var division = 49 / 7;
+
+
+
+console.log(sum)
+console.log(difference)
+console.log(multiply)
+console.log(division)
+
+
+// increment or decrement of number
+
+var a = 20;
+
+var b = 30;
+
+//var a = a +1; // one way of increment
+ //var b = b -10;
+
+a++
+
+b--
+console.log(a) 
+console.log(b)
+
+
+var remainder = 23 % 10; // remainder // remainder 
+
+console.log(remainder)
+
+
+
+
+var num1= 2;
+var num2 = 4;
+
+// var num1 = num1 + 2;
+ num1+=2;
+ num2-=3;
+
+console.log(num1)
+console.log(num2)
+
+
+
+// escaping quote lietrals in java script
+
+var myStr = " i am a \"double quote \" inside a \"double quote \" "  // use / 
+
+console.log(myStr)
+
+
+// using different escape method
+
+var checkStr = "Firstline\n\tSecond lin\n\t\\\nthird line\f"
+
+console.log(checkStr)
+
+
+var strr = " hi there" + " how are you "
+
+console.log(strr)
+
+
